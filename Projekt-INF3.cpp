37f2273d387acacaa0ca804785987870be99b804
Lia S <liia.samkharadz3@gmail.com>
2021-01-19 20:08:40 +0100

    Základ kódu (2nd commit)

--- Projekt-INF3.cpp
+++ Projekt-INF3.cpp
@@ -1,20 +1,41 @@
-// Projekt-INF3.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <sstream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    string answer; 
+    int g;
+
+    cout << "Vyber si cislo a stiskni libovolne tlacitko pro pokracovani. \n" ;
+    cin;
+
+    for (int i = 500; i = g ; i = i/2)
+    {
+        cout << "Je tvoje cislo vetsi nez" << i << "?" << endl;
+
+        cin >> answer;
+        if (answer == "ne")
+        {
+            g = i / 2;
+        }
+        else if (answer == "ano")
+        {
+            g = i + i / 2;
+        }
+
+        else
+        {
+            cout << "Prosim odpovidejte ve tvaru ano/ne. Hra se restartuje. \n";
+        }
+
+        if (i == g)
+        {
+            cout << "tvoje cislo je" << i << ". \n";
+        }
+    }
+
+
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
